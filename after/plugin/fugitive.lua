380095c66c51bfd90afb9010cedb48ef6427662f
RHL120 <rhl120@protonmail.com>
2024-07-11 07:50:41 +0200

    Map Gvdiffsplit on <leader>gp

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,6 +2,6 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gc", function()
 	vim.cmd("G commit")
 end);
-vim.keymap.set("n", "<leader>gd", function()
+vim.keymap.set("n", "<leader>gp", function()
 	vim.cmd("Gvdiffsplit")
 end);
